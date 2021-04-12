29296a6bbf87f31f04611fd6b95bc82ac11a972f
Shawn Yu <shawnyudesign@gmail.com>
2021-04-12 05:26:27 -0400

    jello

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function generateSpots(){
         // currentcolor=color;
 
        for(let i=0;i<$(spots).length;i++){
-           console.log(i)
+        //    console.log(i)
            setTimeout(() => {
                $(spots[i]).css('background-color','hsla('+currentDegree+','+currentSaturation+'%, 70%,100%)')
                
@@ -110,11 +110,11 @@ function generateTentacles4(e){
 } 
 
 $('body').mousemove(e=>{
-    if(e.pageY<currentY){
+    if(e.pageY<currentY-10){
         $('.jellyfishhead').removeClass('flapdown');
         $('.jellyfishhead').addClass('flapup');
     }
-    else if(e.pageY>currentY){
+    else if(e.pageY>currentY+20){
         $('.jellyfishhead').removeClass('flapup')
         $('.jellyfishhead').addClass('flapdown');
     }
@@ -129,11 +129,127 @@ $('body').mousemove(e=>{
     generateTentacles3(e)
     generateTentacles4(e)
     generateTentacles5(e)
-    // document.body.scrollTop=e.pageY+100;
+    $(window).scrollTop(e.pageY-500)
+  
+    // console.log('scrolltop')
+    // console.log(body.scrollTop)
 
     
 })
 
+function generateRocks(){
+    for(i=0;i<20;i++){
+        var randSize =Math.floor( Math.random()*500);
+        var top = Math.random()*100;
+        var rotation=Math.random()*90;
+        var rock = $('<dv>')
+        rock.addClass('rock')
+        rock.css('height',randSize)
+        rock.css('width',randSize)
+        rock.css('top',top+"%")
+        rock.css('transform','rotate('+rotation+'deg)')
+        $('.leftStone').append(rock)
+     }
+}
+function generateRocks2(){
+    for(i=0;i<20;i++){
+        var randSize =Math.floor( Math.random()*500);
+        var top = Math.random()*100;
+        var rotation=Math.random()*90;
+        var rock = $('<dv>')
+        rock.addClass('rock')
+        rock.css('height',randSize)
+        rock.css('width',randSize)
+        rock.css('top',top+"%")
+        rock.css('transform','rotate('+rotation+'deg)')
+        $('.rightStone').append(rock)
+     }
+}
+
+function generateFish(){
+    var school = $('<div>')
+    school.addClass('fishPool')
+    var schoolTop=Math.random()*100
+    school.css('top',schoolTop+'%');
+    var counter = 20
+    
+    for(let i=0;i<20;i++){
+        
+        var randColor = Math.random()*360;
+        var top = Math.random()*100;
+        var left = Math.random()*100;
+        var fish = $('<div>')
+        fish.addClass('fish')
+        fish.css('top',top+"%")
+        fish.css('left',left+"%")
+        fish.css('background-color','hsla('+randColor+',100%, 70%,100%)')
+        school.append(fish);
+        counter--
+}
+    if (counter<1){
+        console.log('fish')
+        $('.water').prepend(school)
+    }
+
+}
+
+function generateFish2(){
+    var school = $('<div>')
+    school.addClass('fishPool2')
+    var schoolTop=Math.random()*100
+    school.css('top',schoolTop+'%');
+    var counter = 20
+    
+    for(let i=0;i<20;i++){
+        
+        var randColor = Math.random()*360;
+        var top = Math.random()*100;
+        var left = Math.random()*100;
+        var fish = $('<div>')
+        fish.addClass('fish')
+        fish.css('top',top+"%")
+        fish.css('left',left+"%")
+        fish.css('background-color','hsla('+randColor+',100%, 70%,100%)')
+        school.append(fish);
+        counter--
+}
+    if (counter<1){
+        console.log('fish')
+        $('.water').prepend(school)
+    }
+
+}
+
+// $('.water').click( e=>{
+//     var scroll=e.pageY
+
+//     $(window).scrollTop(scroll)
+//     console.log(e.pageY)
+//     console.log('scrolltop')
+//     console.log($(window).scrollTop())
+   
+
+// })
+
 
 
 generateSpots()
+generateRocks()
+generateRocks2()
+
+setInterval(() => {
+$('.fishPool').remove()
+generateFish()
+    
+}, 8000);
+
+
+setTimeout(() => {
+setInterval(() => {
+        $('.fishPool2').remove()
+        generateFish2()
+            
+        }, 8000);
+        
+    
+}, 4000);
